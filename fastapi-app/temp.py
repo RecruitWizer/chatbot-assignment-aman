fc9d7838cfddfedb0cab3987f606c0b616fd4829
Aman Adatia <adatiaaman@gmail.com>
2024-01-18 12:39:48 +0530

    stage 1

--- fastapi-app/temp.py
+++ fastapi-app/temp.py
@@ -1,6 +1,6 @@
 from llama_index import GPTSimpteVectorIndex, SimpleDirectoryReader
 
-documents = SimpleDirectoryReader('data').load_data()
+documents = SimpleDirectoryReader('./data').load_data()
 index = GPTSimpteVectorIndex.from_documents(documents)
 response = index.query("What did the author do growing up?" )
 print(response)
